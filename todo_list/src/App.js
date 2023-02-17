00ec190732ae6b18f95c02109afbcb0644d89faa
David Young <davidyoung562@gmail.com>
2023-02-17 04:01:27 -0500

    add and delete todos

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -3,11 +3,22 @@ import "./App.css";
 const App = () => {
   const [todos, setTodos] = React.useState([]);
   const [todo, setTodo] = React.useState("");
+  const [errMsg, setErrMsg] = React.useState("");
   
-  // Add the handlesubmit code here
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (todo.trim().length > 0) {
+            setErrMsg(null);
+            setTodos([...todos, {id: Math.floor(Math.random()*Math.pow(10, 100)), text: todo}]);
+        } else {
+            setErrMsg("NOPE!");
+        }
+        setTodo("");
+    }
   
-  
-  // Add the deleteToDo code here
+    function deleteTodo (e, todo) {
+        setTodos(todos.filter(t => t.id != todo.id));
+    }
 
   
   // Add the toggleComplete code here
@@ -18,11 +29,15 @@ const App = () => {
   
 return(
 <div className ="App">
-<h1>Todo List</h1>
-<form>
-<input type ="text" align ="right" />
-<button type ="submit">Add Todo</button>
-</form>
+    <h1>Todo List</h1>
+    <form onSubmit={handleSubmit}>
+        <input type ="text" align ="right" value={todo} onChange={e => {setTodo(e.target.value);}}/>
+        <p>{errMsg}</p>
+        <button type ="submit">Add Todo</button>
+    </form>
+    <ul>
+        {todos.map(todo => <li>{todo.text} <a onClick={e => deleteTodo(e, todo)}>(x)</a></li>)}
+    </ul>
 </div>
 );
 };
